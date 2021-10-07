7f0fe4ce972bd35f258648903d672bda0d1c57a0
Fabian Ramirez <fabian.ramirez-p@mail.escuelaing.edu.co>
2021-10-06 20:12:39 -0500

    Implement Check Boxes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
+import { TaskItem } from "./components/TaskItem";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      isCompleted: true,
+      name: "Learn React"
+    },
+    {
+      isCompleted: true,
+      name: "Learn Hooks"
+    },
+    {
+      isCompleted: false,
+      name: "Deploy App"
+    }
+  ]);
+  const handleTaskChange = (index) => () => {
+    console.log("changed:" + index);
+    const arr = [...tasks];
+    arr[index].isCompleted = !arr[index].isCompleted;
+    setTasks(arr);
+  };
   return (
- <h1>
-   reactapp
- </h1>
+    <main className="App-main">
+      <form>
+        <input type="text" placeholder="Task name" />
+        <button>Create Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => {
+          return (
+            <TaskItem
+              isChecked={task.isCompleted}
+              taskName={task.name}
+              onTaskChange={handleTaskChange(index)}
+            />
+          );
+        })}
+      </ul>
+    </main>
   );
 }
 
